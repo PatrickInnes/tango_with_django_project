3c483b2ec47d77f37da24c4728e98122fa33c580
Patrick Innes <2523573i@student.gla.ac.uk>
2022-02-04 09:31:42 +0000

    Chapter 9

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from rango.models import Category,Page 
 from rango.forms  import CategoryForm
 from django.shortcuts import redirect,reverse
-from rango.forms import PageForm
+from django.urls import reverse 
+from rango.forms import PageForm,UserForm,UserProfileForm
+from django.contrib.auth import authenticate, login,logout
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
@@ -13,10 +16,19 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list 
     context_dict['pages'] = pages_list
-    return render(request,'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    
+    response = render(request, 'rango/index.html', context=context_dict)
+    visit_cookie_handler(request,response)
+    return response
     
 def about(request):
     context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+    if request.session.test_cookie_worked():
+       print("Test COOKIE WORKED!")
+       request.session.delete_test_cookie()
+       
     return render(request,'rango/about.html')
 
 def show_category(request,category_name_slug):
@@ -30,7 +42,7 @@ def show_category(request,category_name_slug):
        context_dict['pages'] = None
        context_dict['category'] = None 
     return render(request, 'rango/category.html', context=context_dict)
-
+@login_required
 def add_category(request):
      form = CategoryForm()
      if request.method == 'POST':
@@ -43,7 +55,7 @@ def add_category(request):
         
      return render(request, 'rango/add_category.html',{'form':form})
      
-     
+@login_required    
 def add_page(request, category_name_slug):
       try:
           category = Category.objects.get(slug=category_name_slug)
@@ -68,8 +80,82 @@ def add_page(request, category_name_slug):
          else:
             print(form.errors)
       context_dict={'form':form,'category':category}
-      return render(request,'rango/add_page.html',context=context_dict)
-def base(request):
-     return render(request,'rango/base.html')   
-def categories(request):
-     return render(request,'rango/categories.html')     
+      return render(request,'rango/add_page.html',context=context_dict) 
+
+def register(request):
+    registered= False
+    
+    if request.method == 'POST':
+         user_form = UserForm(request.POST)
+         profile_form = UserProfileForm(request.POST)
+         
+         
+         if user_form.is_valid() and profile_form.is_valid():
+               
+               
+               user = user_form.save()
+               user.set_password(user.password)
+               user.save()
+               profile = profile_form.save(commit=False)
+               profile.user = user
+               
+               if 'picture' in request.FILES:
+                    profile.picture = request.FILES['pictures']
+                    
+               profile.save()
+               
+               registered = True
+         else:
+              print(user_form.errors, profile_form.errors)
+              
+               
+    else:
+         user_form = UserForm()
+         profile_form = UserProfileForm
+    return render(request,'rango/register.html',context={'user_form':user_form,'profile_form':profile_form,'registered':registered})
+    
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username,password=password)
+        
+        if user:
+            if user.is_active:
+                login(request,user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled")
+        else:
+             print(f"Invalid login details: {username}, {password}")
+             return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
+@login_required
+def restricted(request):
+    return render(request,'rango/restricted.html')      
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))    
+
+def visitor_cookie_handler(request, response):
+      visits = int(get_server_side_cookie(request,'visits','1'))
+      
+      last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+      last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+      
+      if (datetime.now() - last_visit_time).days > 0:
+         visits += 1
+         request.session['last_visit'] =  str(datetime.now())
+      else:
+         request.session['lave_visit'] = last_visit_cookie 
+      request.session['visits'] = visits 
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+     val = request.session.get(cookie)
+     if not val:
+          val = default_val 
+     return val 
+     
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
@@ -104,8 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
+PASSWORD_HASHERS = ( 'django.contrib.auth.hashers.PBKDF2PasswordHasher','django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',)
+LOGIN_URL= 'rango:login'
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -119,6 +120,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
